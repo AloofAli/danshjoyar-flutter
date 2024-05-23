da34d8fe0c1138470933914ae1b838415353b216
Ali Mahdavi <154345431+AloofAli@users.noreply.github.com>
2024-05-23 13:48:21 +0330

    Add bottomNavigatonButton merge two file

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -7,21 +8,43 @@ void main() {
 
 class LoginApp extends StatelessWidget {
   const LoginApp({super.key});
+
   @override
   Widget build(BuildContext context) {
+
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'DaneshjoYar',
-      home: LoginPage(),
+      debugShowCheckedModeBanner:false,
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          bottomNavigationBar: TabBar(
+            labelColor: Colors.cyan,
+            indicatorColor: Colors.cyanAccent,
+
+            tabs: [
+              Tab(icon: Icon(Icons.login)),
+              Tab(icon: Icon(Icons.add))
+            ],
+          ),
+          body: TabBarView(
+            children: [
+              LoginPage(),
+              SignUpPage(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
+
 
 class _LoginPageState extends State<LoginPage> {
   bool _passwordVisible = false;
@@ -30,125 +53,241 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController passwordController = TextEditingController();
 
   @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    usernameController.dispose();
-    passwordController.dispose();
-
-  }
-  @override
   void initState() {
+    super.initState();
     _passwordVisible = false;
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   centerTitle: true,
-      //   title: const Text(
-      //     'Login',
-      //     softWrap: true,
-      //     style: TextStyle(
-      //       fontStyle: FontStyle.italic,
-      //       fontSize: 30,
-      //       fontWeight: FontWeight.bold,
-      //       color: Colors.black,
-      //     ),
-      //   ),
-      // ),
       body: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("lib/asset/images/alex-shutin-kKvQJ6rK6S4-unsplash.jpg"),
-                fit: BoxFit.cover,)),
-          padding: const EdgeInsets.fromLTRB(30,60,30,30),
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                  "lib/asset/images/alex-shutin-kKvQJ6rK6S4-unsplash.jpg"),
+              fit: BoxFit.cover,
+              filterQuality: FilterQuality.high,
+            )),
+        padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
         child: Column(
-
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-
-        children: [
-           Image.asset("lib/asset/images/Sbu-logo.svg.png",
-            scale: MediaQuery.of(context).size.width/55
-      ),
-          TextField(
-            controller: usernameController,
-            cursorColor: Colors.cyan,
-            decoration: const InputDecoration(
-              labelText: 'Username',
-              labelStyle: TextStyle(fontSize: 20.0, color: Colors.white),
-              hintText: 'Enter your username',
-              hintStyle: TextStyle(fontSize: 20.0, color: Colors.white70),
-
-              icon: Icon(Icons.account_circle_sharp),
-              iconColor: Colors.white,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.asset(
+              "lib/asset/images/Sbu-logo.svg.png",
+              scale: MediaQuery
+                  .of(context)
+                  .size
+                  .width / 60,
+              filterQuality: FilterQuality.high,
             ),
-          ),
-          // const SizedBox(height: 44),
-          TextFormField(
-            keyboardType: TextInputType.text,
-            controller: passwordController,
-            obscureText: !_passwordVisible,
-            //This will obscure text dynamically
-            decoration: InputDecoration(
-              labelText: 'Password',
-              labelStyle: TextStyle(fontSize: 20.0, color: Colors.white),
-              hintText: 'Enter your password',
-              hintStyle: TextStyle(fontSize: 20.0, color: Colors.white70),
-              icon: Icon(Icons.key),
-              iconColor: Colors.white,
-              // Here is key idea
-              suffixIcon: IconButton(
-                splashColor: Colors.white,
-                tooltip: "Change visibility",
-                icon: Icon(
-                  // Based on passwordVisible state choose the icon
-                  _passwordVisible ? Icons.visibility_off : Icons.visibility,
-                  color: Theme
-                      .of(context)
-                      .dialogBackgroundColor,
-                ),
-                onPressed: () {
-                  // Update the state i.e. toogle the state of passwordVisible variable
-                  setState(() {
-                    _passwordVisible = !_passwordVisible;
-                  });
-                },
+
+            TextField(
+              controller: usernameController,
+              cursorColor: Colors.cyan,
+              decoration: const InputDecoration(
+                labelText: 'Username',
+                labelStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+                hintText: 'Enter your username',
+                hintStyle: TextStyle(fontSize: 20.0, color: Colors.white70),
+                icon: Icon(Icons.account_circle_sharp),
+                iconColor: Colors.white,
               ),
             ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(shape: const StadiumBorder(),
-            backgroundColor:Colors.black12),
-            onPressed: () {
-              String username = usernameController.text;
-              String password = passwordController.text;
-            },
-            child: const Text('Login',
-                style: TextStyle(
-                  letterSpacing: 3.5,
-                  color: Colors.cyan,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                )),
-          ),
-
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              shape: const StadiumBorder(),
-              backgroundColor:Colors.black12,
+            // const SizedBox(height: 44),
+            TextFormField(
+              keyboardType: TextInputType.text,
+              controller: passwordController,
+              obscureText: !_passwordVisible,
+              //This will obscure text dynamically
+              decoration: InputDecoration(
+                labelText: 'Password',
+                labelStyle: const TextStyle(fontSize: 20.0, color: Colors.white),
+                hintText: 'Enter your password',
+                hintStyle: const TextStyle(fontSize: 20.0, color: Colors.white70),
+                icon: const Icon(Icons.key),
+                iconColor: Colors.white,
+                // Here is key idea
+                suffixIcon: IconButton(
+                  splashColor: Colors.white,
+                  tooltip: "Change visibility",
+                  icon: Icon(
+                    // Based on passwordVisible state choose the icon
+                    _passwordVisible ? Icons.visibility_off : Icons.visibility,
+                    color: Theme
+                        .of(context)
+                        .dialogBackgroundColor,
+                  ),
+                  onPressed: () {
+                    // Update the state i.e. toogle the state of passwordVisible variable
+                    setState(() {
+                      _passwordVisible = !_passwordVisible;
+                    });
+                  },
+                ),
+              ),
             ),
-            onPressed: () {},
-            child: const Text('Register',
-                style: TextStyle(
-                  letterSpacing: 3.5,
-                  color: Colors.cyan,
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                )),
-          )
-        ],
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                  backgroundColor: Colors.black12),
+              onPressed: () {
+                String username = usernameController.text;
+                String password = passwordController.text;
+              },
+              child: const Text('Login',
+                  style: TextStyle(
+                    letterSpacing: 3.5,
+                    color: Colors.cyan,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ),
+          ],
+        ),
       ),
-    ),);
+    )
+    ;
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+}
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  bool _passwordVisible = false;
+
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController studentIDController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _passwordVisible = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                "lib/asset/images/alex-shutin-kKvQJ6rK6S4-unsplash.jpg",
+              ),
+              fit: BoxFit.fill,
+            )),
+        padding: const EdgeInsets.fromLTRB(30, 30, 30, 0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset("lib/asset/images/Sbu-logo.svg.png",
+                  scale: MediaQuery.of(context).size.width / 60),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                controller: usernameController,
+                cursorColor: Colors.white,
+                cursorOpacityAnimates: true,
+                decoration: const InputDecoration(
+                  labelText: 'Username',
+                  labelStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+                  hintText: 'Enter your username',
+                  hintStyle: TextStyle(fontSize: 20.0, color: Colors.white70),
+                  icon: Icon(Icons.account_circle_sharp),
+                  iconColor: Colors.white,
+                ),
+              ),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                controller: studentIDController,
+                cursorColor: Colors.white,
+                cursorOpacityAnimates: true,
+                decoration: const InputDecoration(
+                  labelText: 'StudentID',
+                  labelStyle: TextStyle(fontSize: 20.0, color: Colors.white),
+                  hintText: 'Enter your StudentID',
+                  hintStyle: TextStyle(fontSize: 20.0, color: Colors.white70),
+                  icon: Icon(Icons.account_circle_sharp),
+                  iconColor: Colors.white,
+                ),
+              ),
+              // SizedBox(height:0),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                controller: passwordController,
+                obscureText: !_passwordVisible,
+                cursorColor: Colors.white,
+                cursorOpacityAnimates: true,
+                decoration: InputDecoration(
+                  helperText: "Create a password with at least 8 characters",
+                  helperStyle: const TextStyle(color: Colors.black),
+                  labelText: 'Create Password',
+                  labelStyle:
+                  const TextStyle(fontSize: 20.0, color: Colors.white),
+                  hintText: 'Enter your password',
+                  hintStyle:
+                  const TextStyle(fontSize: 20.0, color: Colors.white70),
+                  icon: Icon(Icons.key),
+                  iconColor: Colors.white,
+                  // Here is key idea
+                  suffixIcon: IconButton(
+                    splashColor: Colors.white,
+                    tooltip: "Change visibility",
+                    icon: Icon(
+                      // Based on passwordVisible state choose the icon
+                      _passwordVisible ? Icons.visibility_off : Icons.visibility,
+                      color: Theme.of(context).dialogBackgroundColor,
+                    ),
+                    onPressed: () {
+                      // Update the state i.e. toogle the state of passwordVisible variable
+                      setState(() {
+                        _passwordVisible = !_passwordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+
+              // const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: const StadiumBorder(),
+                  backgroundColor: Colors.black12,
+                ),
+                onPressed: () {},
+                child: const Text('Register',
+                    style: TextStyle(
+                      letterSpacing: 3.5,
+                      color: Colors.cyan,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    )),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    studentIDController.dispose();
+    super.dispose();
   }
 }
